d6d568a6c9c2c18db6c0525f8208eb2060d6430b
s.aran <N/A>
2021-08-05 00:57:58 +0900

    replace write(ln) to logger and add fatal error

--- source/VersionInfo.d
+++ source/VersionInfo.d
@@ -3,9 +3,9 @@ struct VersionInfo
 {
   static auto Major = "0";
   static auto Minor = "21";
-  static auto Build = "293";
-  static auto Hash = "078b3c4";
-  static auto Datetime = "2021/07/19 00:17:06";
-  static auto VersionString = "0.21.293 (078b3c4)";
+  static auto Build = "330";
+  static auto Hash = "6d3d598";
+  static auto Datetime = "2021/08/04 01:16:03";
+  static auto VersionString = "0.21.330 (6d3d598)";
 }
 
--- source/github.d
+++ source/github.d
@@ -6,6 +6,10 @@ import std.array : join, replace;
 import std.conv;
 import std.string;
 import std.stdio;
+import std.format;
+import core.stdc.stdlib: exit;
+
+import Logger;
 
 class GitHub
 {
@@ -16,22 +20,34 @@ class GitHub
 
 	JSONValue[] get_releases(string repository)
 	{
+		auto logger = EzLogger.get_logger("http");
+
 		auto client = HTTP();
 		client.addRequestHeader("Accept", "application/vnd.github.v3+json");
 
 		auto release_url = this.create_url(repository);
-		char[] res = get(release_url, client);
-		string sres = to!string(res);
 
-		return parseJSON(sres).array;
+		try
+		{
+			char[] res = get(release_url, client);
+			string sres = to!string(res);
+			return parseJSON(sres).array;
+		}
+		catch (std.net.curl.HTTPStatusException e)
+		{
+			logger.fatal(format("%s:%d: %s", e.file, e.line, e.msg));
+			exit(255);
+		}
 	}
 
 	bool download(string browser_download_url, string to)
 	{
+		auto logger = EzLogger.get_logger("http");
+
 		auto client = HTTP();
-		write(browser_download_url);
+		logger.info(browser_download_url);
 		std.net.curl.download(browser_download_url, to, client);
-		writeln(" ... Done");
+		logger.info(" ... Done");
 		return true;
 	}
 }
