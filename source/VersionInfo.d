cad00ccc44e14e2f404b63d776c148c37a9f5299
s.aran <N/A>
2021-08-06 00:23:30 +0900

    release v0.21

--- source/VersionInfo.d
+++ source/VersionInfo.d
@@ -3,9 +3,9 @@ struct VersionInfo
 {
   static auto Major = "0";
   static auto Minor = "21";
-  static auto Build = "339";
-  static auto Hash = "8f4d34e";
-  static auto Datetime = "2021/08/06 00:15:09";
-  static auto VersionString = "0.21.339 (8f4d34e)";
+  static auto Build = "340";
+  static auto Hash = "b5e2be8";
+  static auto Datetime = "2021/08/06 00:20:18";
+  static auto VersionString = "0.21.340 (b5e2be8)";
 }
 
