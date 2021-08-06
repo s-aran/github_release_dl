77b1b9a2da7e2bb63af9f4d84b1f55941b40fb48
s.aran <s-aran@users.noreply.github.com>
2021-08-07 00:38:58 +0900

    add help option

--- source/VersionInfo.d
+++ source/VersionInfo.d
@@ -3,9 +3,9 @@ struct VersionInfo
 {
   static auto Major = "0";
   static auto Minor = "21";
-  static auto Build = "341";
-  static auto Hash = "ec50073";
-  static auto Datetime = "2021/08/06 00:37:23";
-  static auto VersionString = "0.21.341 (ec50073)";
+  static auto Build = "344";
+  static auto Hash = "342cb54";
+  static auto Datetime = "2021/08/07 00:26:37";
+  static auto VersionString = "0.21.344 (342cb54)";
 }
 
--- source/app.d
+++ source/app.d
@@ -12,6 +12,7 @@ import std.zip;
 import std.array;
 import std.getopt;
 import std.format;
+import core.stdc.stdlib: exit;
 
 import VersionInfo;
 import GitHub;
@@ -22,152 +23,176 @@ import Logger;
 
 class Config
 {
-	static string config_filepath = "package.json";
-	static EzLogger.Level log_level = EzLogger.Level.Info;
+  static string config_filepath = "package.json";
+  static EzLogger.Level log_level = EzLogger.Level.Critical;
 }
 
 void main(string[] args)
 {
-	auto logger = EzLogger.get_logger("main");
-	logger.info("github release downloader");
-	logger.info(format("version: %s", VersionInfo.VersionInfo.VersionString));
+  auto logger = EzLogger.get_logger("main");
+  EzLogger.set_all_level(Config.log_level);
 
-	void optionCallback(string option)
-	{
-		logger.info(format("option=%s", option));
-	}
+  logger.info("github release downloader");
+  logger.info(format("version: %s", VersionInfo.VersionInfo.VersionString));
 
-	void loglevel_callback(string op)
-	{
-		switch (op)
-		{
-			case "trace":
-				Config.log_level = EzLogger.Level.Trace;
-				break;
-			case "info":
-				Config.log_level = EzLogger.Level.Info;
-				break;
-			case "warn":
-			case "warning":
-				Config.log_level = EzLogger.Level.Warn;
-				break;
-			case "error":
-				Config.log_level = EzLogger.Level.Error;
-				break;
-			case "critical":
-				Config.log_level = EzLogger.Level.Critical;
-				break;
-			case "fatal":
-				Config.log_level = EzLogger.Level.Fatal;
-				break;
-			default:
-				return;
-		}
-		EzLogger.set_all_level(Config.log_level);
-	}
-	getopt(args, 
-			"trace", &loglevel_callback, 
-			"info", &loglevel_callback, 
-			"warn", &loglevel_callback, 
-			"warning", &loglevel_callback, 
-			"error", &loglevel_callback, 
-			"critical", &loglevel_callback, 
-			"fatal", &loglevel_callback
-		);
+  void package_json_file_callback(string path)
+  {
+    Config.config_filepath = path;
+  }
 
-	auto package_configure = new PackageConfigure.PackageConfigure(Config.config_filepath);
-	package_configure.load();
-	foreach (i, p; package_configure.packages)
-	{
-		auto info = p.info;
+  void help_callback()
+  {
+      writeln("github_release_dl [--trace][--info][--warn][--error][--critical][--fatal][--package package_json]");
+      writeln("");
+      writeln("Logger parameters:");
+      writeln("    --trace    ... set log level to trace");
+      writeln("    --info     ... set log level to info");
+      writeln("    --warn     ... set log level to warn");
+      writeln("    --error    ... set log level to error");
+      writeln("    --critical ... set log level to critical (default)");
+      writeln("    --fatal    ... set log level to fatal");
+      writeln("");
+      writeln("Setting parameter:");
+      writeln("    --package package_json");
+      writeln("               ... specify package.json path (default: package.json)");
 
-		auto repository = info.repository;
-		auto install = info.install;
-		auto filename = info.filename;
-		const auto destination = info.destination;
-		const auto rename = info.rename;
+      exit(0);
+  }
 
-		auto github = new GitHub.GitHub();
-		JSONValue[] res_doc = github.get_releases(repository);
+  void loglevel_callback(string op)
+  {
+    switch (op)
+    {
+      case "trace":
+        Config.log_level = EzLogger.Level.Trace;
+        break;
+      case "info":
+        Config.log_level = EzLogger.Level.Info;
+        break;
+      case "warn":
+      case "warning":
+        Config.log_level = EzLogger.Level.Warn;
+        break;
+      case "error":
+        Config.log_level = EzLogger.Level.Error;
+        break;
+      case "critical":
+        Config.log_level = EzLogger.Level.Critical;
+        break;
+      case "fatal":
+        Config.log_level = EzLogger.Level.Fatal;
+        break;
+      default:
+        return;
+    }
+    EzLogger.set_all_level(Config.log_level);
+  }
 
-		auto latest_release = res_doc[0];
-		JSONValue[string] e = latest_release.object;
-		const auto published_at = PackageConfigure.PackageConfigure.string_to_datetime(e["published_at"].str);
-		JSONValue[] assets = e["assets"].array;
-		foreach (a; assets)
-		{
-			JSONValue[string] n = a.object;
-			string name = n["name"].str;
+  getopt(args, 
+      "help", &help_callback,
+      "trace", &loglevel_callback, 
+      "info", &loglevel_callback, 
+      "warn", &loglevel_callback, 
+      "warning", &loglevel_callback, 
+      "error", &loglevel_callback, 
+      "critical", &loglevel_callback, 
+      "fatal", &loglevel_callback,
+      "package", &package_json_file_callback
+    );
 
-			string dir = "tmp";
-			const auto match = matchFirst(name, filename);
+  auto package_configure = new PackageConfigure.PackageConfigure(Config.config_filepath);
+  package_configure.load();
+  foreach (i, p; package_configure.packages)
+  {
+    auto info = p.info;
 
-			// | match | newer | !exists | download |
-			// |-------|-------|---------|----------|
-			// | false | false | false   | false    |
-			// | false | false | true    | false    |
-			// | false | true  | false   | false    |
-			// | false | true  | true    | false    |
-			// | true  | false | false   | false    |
-			// | true  | false | true    | true     |
-			// | true  | true  | false   | true     |
-			// | true  | true  | true    | true     |
-			if (!match)
-			{
-				continue;
-			}
+    auto repository = info.repository;
+    auto install = info.install;
+    auto filename = info.filename;
+    const auto destination = info.destination;
+    const auto rename = info.rename;
 
-			logger.trace(name);
+    auto github = new GitHub.GitHub();
+    JSONValue[] res_doc = github.get_releases(repository);
 
-			string dl_dest_path = buildPath(dir, name);
-			if (!(install > published_at || !exists(dl_dest_path)))
-			{
-				break;
-			}
+    auto latest_release = res_doc[0];
+    JSONValue[string] e = latest_release.object;
+    const auto published_at = PackageConfigure.PackageConfigure.string_to_datetime(e["published_at"].str);
+    JSONValue[] assets = e["assets"].array;
+    foreach (a; assets)
+    {
+      JSONValue[string] n = a.object;
+      string name = n["name"].str;
 
-			FileUtils.mkdir_if_not_exists(dir);
+      string dir = "tmp";
+      const auto match = matchFirst(name, filename);
 
-			string download_url = n["browser_download_url"].str;
-			github.download(download_url, dl_dest_path);
+      // | match | newer | !exists | download |
+      // |-------|-------|---------|----------|
+      // | false | false | false   | false    |
+      // | false | false | true    | false    |
+      // | false | true  | false   | false    |
+      // | false | true  | true    | false    |
+      // | true  | false | false   | false    |
+      // | true  | false | true    | true     |
+      // | true  | true  | false   | true     |
+      // | true  | true  | true    | true     |
+      if (!match)
+      {
+        continue;
+      }
 
-			auto extract_to = destination.length > 0 ? destination : dir;
-			FileUtils.mkdir_if_not_exists(extract_to);
+      logger.trace(name);
 
-			const auto file_type = FileUtils.analyze(dl_dest_path);
-			logger.trace(format("file_type = %s", file_type));
-			auto result = false;
-			switch (file_type)
-			{
-			case FileUtils.FileType.Zip:
-				logger.trace(format("zip dir: %s", FileUtils.dirname_zip(dl_dest_path)));
-				if (rename.length > 0)
-				{
-					// extract to BaseDirectory
-					result = FileUtils.extract_zip(dl_dest_path, dir);
-					auto from_path = buildPath(dir, FileUtils.dirname_zip(dl_dest_path));
-					auto to_path = buildPath(destination, rename);
-					logger.trace(format("from = %s, to=%s", from_path, to_path));
-					FileUtils.move_recurse(from_path, to_path);
-				}
-				else
-				{
-					result = FileUtils.extract_zip(dl_dest_path, extract_to);
-				}
-				break;
-			case FileUtils.FileType.Exe:
-				// fall through
-			case FileUtils.FileType.Msi:
-				result = FileUtils.execute(dl_dest_path, "");
-				break;
-			default:
-				// download only
-				result = true;
-				break;
-			}
-		}
+      string dl_dest_path = buildPath(dir, name);
+      if (!(install > published_at || !exists(dl_dest_path)))
+      {
+        break;
+      }
 
-		package_configure.packages[i].info.install = published_at;
-	}
+      FileUtils.mkdir_if_not_exists(dir);
 
-	package_configure.save();
+      string download_url = n["browser_download_url"].str;
+      github.download(download_url, dl_dest_path);
+
+      auto extract_to = destination.length > 0 ? destination : dir;
+      FileUtils.mkdir_if_not_exists(extract_to);
+
+      const auto file_type = FileUtils.analyze(dl_dest_path);
+      logger.trace(format("file_type = %s", file_type));
+      auto result = false;
+      switch (file_type)
+      {
+      case FileUtils.FileType.Zip:
+        logger.trace(format("zip dir: %s", FileUtils.dirname_zip(dl_dest_path)));
+        if (rename.length > 0)
+        {
+          // extract to BaseDirectory
+          result = FileUtils.extract_zip(dl_dest_path, dir);
+          auto from_path = buildPath(dir, FileUtils.dirname_zip(dl_dest_path));
+          auto to_path = buildPath(destination, rename);
+          logger.trace(format("from = %s, to=%s", from_path, to_path));
+          FileUtils.move_recurse(from_path, to_path);
+        }
+        else
+        {
+          result = FileUtils.extract_zip(dl_dest_path, extract_to);
+        }
+        break;
+      case FileUtils.FileType.Exe:
+        // fall through
+      case FileUtils.FileType.Msi:
+        result = FileUtils.execute(dl_dest_path, "");
+        break;
+      default:
+        // download only
+        result = true;
+        break;
+      }
+    }
+
+    package_configure.packages[i].info.install = published_at;
+  }
+
+  package_configure.save();
 }
