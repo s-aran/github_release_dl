9c7aa76cc81dfd6f68fd202246a3fe12a5527b8b
s.aran <s-aran@users.noreply.github.com>
2021-08-07 01:10:26 +0900

    release v0.22

--- source/VersionInfo.d
+++ source/VersionInfo.d
@@ -3,9 +3,9 @@ struct VersionInfo
 {
   static auto Major = "0";
   static auto Minor = "22";
-  static auto Build = "359";
-  static auto Hash = "77b1b9a";
-  static auto Datetime = "2021/08/07 00:38:58";
-  static auto VersionString = "0.22.359 (77b1b9a)";
+  static auto Build = "360";
+  static auto Hash = "aee140e";
+  static auto Datetime = "2021/08/07 01:07:20";
+  static auto VersionString = "0.22.360 (aee140e)";
 }
 
