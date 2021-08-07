f61626d07637479891ed9c0fe5e7dd744c19d233
s.aran <s-aran@users.noreply.github.com>
2021-08-08 02:14:16 +0900

    release v0.23

--- source/VersionInfo.d
+++ source/VersionInfo.d
@@ -3,9 +3,9 @@ struct VersionInfo
 {
   static auto Major = "0";
   static auto Minor = "23";
-  static auto Build = "386";
-  static auto Hash = "748d2dd";
-  static auto Datetime = "2021/08/08 01:12:46";
-  static auto VersionString = "0.23.386 (748d2dd)";
+  static auto Build = "387";
+  static auto Hash = "b2ebe67";
+  static auto Datetime = "2021/08/08 02:12:50";
+  static auto VersionString = "0.23.387 (b2ebe67)";
 }
 
