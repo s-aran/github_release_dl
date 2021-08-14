094a80ca57b4b5b0ce8d09b4d2f50490a8a5326c
s.aran <s-aran@users.noreply.github.com>
2021-08-15 00:57:21 +0900

    release v0.25

--- source/VersionInfo.d
+++ source/VersionInfo.d
@@ -3,9 +3,9 @@ struct VersionInfo
 {
   static auto Major = "0";
   static auto Minor = "25";
-  static auto Build = "419";
-  static auto Hash = "4a36986";
-  static auto Datetime = "2021/08/14 23:13:23";
-  static auto VersionString = "0.25.419 (4a36986)";
+  static auto Build = "420";
+  static auto Hash = "34f7ace";
+  static auto Datetime = "2021/08/15 00:55:59";
+  static auto VersionString = "0.25.420 (34f7ace)";
 }
 
