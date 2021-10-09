e34a6d7d484bf1239c56b8b9c2173f83b58f554f
s.aran <s-aran@users.noreply.github.com>
2021-10-09 19:01:01 +0900

    release 0.30

--- source/VersionInfo.d
+++ source/VersionInfo.d
@@ -2,10 +2,10 @@ module VersionInfo;
 struct VersionInfo
 {
   static auto Major = "0";
-  static auto Minor = "25";
-  static auto Build = "457";
-  static auto Hash = "c7b1855";
-  static auto Datetime = "2021/10/09 18:54:35";
-  static auto VersionString = "0.25.457 (c7b1855)";
+  static auto Minor = "30";
+  static auto Build = "460";
+  static auto Hash = "7e8d0da";
+  static auto Datetime = "2021/10/09 18:59:25";
+  static auto VersionString = "0.30.460 (7e8d0da)";
 }
 
